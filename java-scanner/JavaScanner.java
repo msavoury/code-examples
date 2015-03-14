65346c657bad0a33308e89ff7cf5611886136e9b
Marcos Savoury <marcossavoury@hotmail.com>
2015-03-14 02:17:44 -0400

    Simple Java Scanner Example

---
+++ java-scanner/JavaScanner.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.Scanner;
+
+/* Examples using the Scanner class
+ * - by default uses whitespace as the delimiter
+ */
+public class JavaScanner {
+    public static void main(String [] args) throws IOException {
+
+        simpleRead();
+        simpleReadWithDelimiter();
+        simpleReadInts();
+    }
+
+    public static void simpleRead() throws IOException {
+        Scanner s = null;
+        try {
+           //s = new Scanner(new File("one-number-per-line.txt"));
+           s = new Scanner(new File("lorem-ipsum.txt"));
+           while (s.hasNext()) {
+               System.out.println(s.next());
+           }
+        }
+        finally {
+            if (s != null) s.close();
+        }
+    }
+
+    public static void simpleReadWithDelimiter() throws IOException {
+        Scanner s = null;
+        try {
+           s = new Scanner(new File("numbers.csv"));
+           s.useDelimiter(",");
+           while (s.hasNext()) {
+               System.out.println(s.next());
+           }
+        }
+        finally {
+            if (s != null) s.close();
+        }
+    }
+
+    public static void simpleReadInts() throws IOException {
+        Scanner s = null;
+        try {
+           s = new Scanner(new File("example.txt"));
+           while (s.hasNext()) {
+               if (s.hasNextInt()) System.out.println(s.nextInt());
+               else s.next();
+           }
+        }
+        finally {
+            if (s != null) s.close();
+        }
+    }
+}
